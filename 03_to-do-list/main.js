d29af939e2d32a82102975d3dd3e50e0cf94e5bd
serenanamee <serena.chang169@gmail.com>
2020-08-12 17:14:05 +0800

    JS-ToDoList

--- 03_to-do-list/main.js
+++ 03_to-do-list/main.js
@@ -1,1 +1,27 @@
-// TO DO
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#addBtn').addEventListener('click', function () {
+    let input = document.getElementById('input').value
+    let listAdd = document.getElementById('list')
+    let liAdd = document.createElement('li')
+    let spanAdd = document.createElement('span')
+
+    liAdd.textContent = input;
+    listAdd.appendChild(liAdd);
+    spanAdd.classList.add('close');
+    liAdd.appendChild(spanAdd);
+    spanAdd.textContent = 'x';
+    liAdd.addEventListener('click', function () {
+      liAdd.classList.toggle("checked");
+    });
+  })
+  let liColor = document.querySelectorAll('li');
+  liColor.forEach((li) => {
+    li.addEventListener('click', function () {
+      li.classList.toggle("checked");
+    });
+  });
+
+})
+
+
+
